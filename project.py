b4fc59f5c45c71cff5918ad582aeba12f2fb3813
Brian Ott <bott@b1-systems.de>
2024-01-27 13:53:58 +0100

    fix: format

--- project.py
+++ project.py
@@ -1,7 +1,9 @@
 import sqlite3
 
+
 def main():
     print(search_db())
+
 
 # Search with a month as input from user
 # Display which vegetables can be raise, planted or harvested for the given month
@@ -12,5 +14,5 @@ def search_db():
     rows = cursor.fetchall()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
