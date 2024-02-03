1df2f1f5426a7486ad8dda97fd27b49f35b5cec8
Brian Ott <bott@b1-systems.de>
2024-02-03 21:44:07 +0100

    added function to create tables and session for app usage

--- app/db/configurations.py
+++ app/db/configurations.py
@@ -10,3 +10,13 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def db_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
