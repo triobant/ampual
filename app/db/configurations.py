4e81022d3a4d0ebdc85a396c723a8cacf891c7e1
Brian Ott <bott@b1-systems.de>
2024-02-04 19:56:10 +0100

    fix: variable in string to create db in same dir

--- app/db/configurations.py
+++ app/db/configurations.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///db/plants.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./plants.db"
 
 
 engine = create_engine(
