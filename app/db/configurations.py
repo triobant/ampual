30f9431e81bba08f56ad52b911f81bd0b1b8aac6
Brian Ott <bott@b1-systems.de>
2024-02-04 20:05:11 +0100

    add: some comments to configurations.py

--- app/db/configurations.py
+++ app/db/configurations.py
@@ -3,19 +3,26 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+# SQLite db created in the same directory
 SQLALCHEMY_DATABASE_URL = "sqlite:///./plants.db"
 
 
+# engine to connect to SQLite db
 engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},echo=True
 )
+
+# represents database session; class itself != a database session
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
+# returns a class to create Base class; later -> inherit form this class to create each db modeles/classes
 Base = declarative_base()
 
 def init_db():
     Base.metadata.create_all(engine)
 
+
+# used to create independent db session for each request and close it afterwards
 def db_session():
     session = SessionLocal()
     try:
