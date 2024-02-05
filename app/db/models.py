ad2f8d01528f2c1b5579c7391709fb6e9dc74666
Brian Ott <bott@b1-systems.de>
2024-02-05 12:55:14 +0100

    add: helper function in models.py

--- app/db/models.py
+++ app/db/models.py
@@ -2,9 +2,15 @@ from sqlalchemy import Column, Integer, String
 from .configurations import Base
 
 
+# Model Class for the plants stored in the db
 class Plant(Base):
     __tablename__ = 'plants'
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
     raising_time = Column(String, index=True)
     transplant_time = Column(String, index=True)
     harvest_time = Column(String, index=True)
+
+    # Helper method to print the object at runtime
+    def __repr__(self):
+        return 'PlantModel(name=%s, raising_time=%s, transplant_time=%s, harvest_time=%s)' % (self.name, self.raising_time, self.transplant_time, self.harvest_time)
