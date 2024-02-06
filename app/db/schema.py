bf38ce96fd7ce646dfc18651df826e2a89ec009b
Brian Ott <bott@b1-systems.de>
2024-02-06 16:32:36 +0100

    add: PlantCreate and config for pydantic parsing

--- app/db/schema.py
+++ app/db/schema.py
@@ -2,8 +2,20 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class PlantSchema(BaseModel):
+class PlantBase(BaseModel):
     name: str
     raising_time: str
     transplant_time: str
     harvest_time: str
+
+
+class PlantCreate(PlantBase):
+    pass
+
+
+class Plant(PlantBase):
+    id: int
+
+
+    class Config:
+        orm_mode = True
