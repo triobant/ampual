2cf06fee609445668e1acf1b3fc53c5d0a68657d
Brian Ott <bott@b1-systems.de>
2024-02-07 19:53:34 +0100

    add: description and version

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from app.db.schema import PlantBase
 from app.db.models import Plant
 
 
-app = FastAPI(title="Ampual - Grow your food")
+app = FastAPI(
+    title="Ampual - Grow your food",
+    description="Ampual - with FastAPI, Tailwind-CSS, React, SQLite3 and SQLAlchemy",
+    version="0.0.1",
+)
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,25 +21,31 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 def read_root():
     return {"Ping": "Pong"}
+
 
 @app.get("/app/api/plants")
 async def get_plants():
     return 1
 
+
 @app.get("/app/api/plants{id}")
 async def get_plants_by_id(id):
     return 2
+
 
 @app.post("/app/api/plants")
 async def post_plants(id):
     return 2
 
+
 @app.put("/app/api/plants{id}")
 async def put_plants(id, data):
     return 2
+
 
 @app.delete("/app/api/plants{id}")
 async def delete_plants(id):
