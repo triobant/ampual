7d335a4abcde2b1b5a56ca283b1e85127ccc7048
Brian Ott <bott@b1-systems.de>
2024-02-16 11:50:00 +0100

    fix: typo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, Depends, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware import CORSmiddleware
+from fastapi.middleware import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from backend.db.configurations import init_db, db_session, engine
