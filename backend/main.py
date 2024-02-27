c5414bf1b33ae81cf15e0416224fd5476fd45f04
Brian Ott <bott@b1-systems.de>
2024-02-27 21:22:08 +0100

    fix: missing slash

--- backend/main.py
+++ backend/main.py
@@ -91,7 +91,7 @@ def get_plant(plant_name: str, db_session: Session = Depends(db_session)):
     return db_plant
 
 
-@router.delete("plants/{plant_name}", tags=["Plant"])
+@router.delete("/plants/{plant_name}", tags=["Plant"])
 async def delete_plants(plant_name: str, db_session: Session = Depends(db_session)):
     """
     Delete the Plant with the given name provided by User stored in database
